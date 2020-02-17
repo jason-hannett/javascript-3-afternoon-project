d741283b778cbd646ab260cad552df65868555bb
jason-hannett <jason.hannett@outlook.com>
2020-02-16 18:56:07 -0700

    fourth commit

--- nesting.js
+++ nesting.js
@@ -49,8 +49,20 @@ var employees = [
     2. If the employee's first name is Lorie, change her department to 'HR'.
     3. Return the updated employee array.
 */
+ function employeeUpdater (){
+   for(var i=0; i<employees.length; i++){
+   if (employees[i].firstName === "Theo"){
+     employees.splice(i,1)
+   } else {
+     if (employees[i].firstName === "Lorie"){
+     employees[i].department = "HR"
+   }}
+ }
+return employees;
+}
 
-//Code Here
+
+// console.log(employeeUpdater)
 
 
 
@@ -68,8 +80,19 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+var removeDuplicates = (arr) => {
+  for(var i = 0; i < arr.length; i++) {
+    for(var j = i + 1; j < arr.length; j++) {
+      if(arr[i] === arr[j]) {
+        arr.splice(i, 1)
+        i--
+      }
+    }
+  }
+  return arr
+}
 
+console.log(removeDuplicates())
 
 
 ////////// PROBLEM 3 //////////
